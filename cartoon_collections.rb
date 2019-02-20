310228c4ad5c94f6e2953d3b1561deaafef6f52c
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 23:14:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each {|x|
+    puts x 
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect {|x|
+    "#{x.capitalize}!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
