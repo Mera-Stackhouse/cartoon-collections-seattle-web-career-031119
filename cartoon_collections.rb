6efa08179462f7a6e27c23b8b12685dd276b1a89
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 23:20:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,14 @@ def summon_captain_planet(array)# code an argument here
   }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|x|
+    x.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
