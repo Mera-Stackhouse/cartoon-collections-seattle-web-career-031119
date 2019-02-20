017eaefdf87464bdbfbf2ee55b27aa5f6df8ed68
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 23:22:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,4 +22,9 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|x|
+    x.include?(cheese_types[0])
+    x.include?(cheese_types[1])
+    x.include?(cheese_types[2])
+  }
 end
